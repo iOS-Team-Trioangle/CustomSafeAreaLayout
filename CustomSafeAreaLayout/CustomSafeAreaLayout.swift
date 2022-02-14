2d87cfb9c2d9a44f55165d32f23108ce8d9c41f6
Kiran <ios@trioangle.com>
2022-02-14 16:00:05 +0530

    New Files Added

---
+++ CustomSafeAreaLayout/CustomSafeAreaLayout.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomSafeAreaLayout
+//
+//  Created by trioangle on 14/02/22.
+//
+
+import UIKit
+
+class CustomSafeAreaLayout: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
